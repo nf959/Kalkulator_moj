0b07b23d3c99fd75d02ce1a7784c234a51de5b5e
Władysław Janicki <30354547+wwj231@users.noreply.github.com>
2019-09-28 15:57:09 +0200

    Zwiekszono petle

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App{
    public static main(Strings[] args){
        System.out.println("Poczatek programu");
-      for(int i = 0 ; i < 10 ; i++){
+      for(int i = 0 ; i < 100 ; i++){
       }
   }
 }
