99f2e218584de56d61093a2372de9c5a1c0ab8b3
nf959 <42828168+nf959@users.noreply.github.com>
2019-09-28 15:44:05 +0200

    COmmit

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App{
    public static main(Strings[] args){
        System.out.println("Poczatek programu");
       for(int i = 0 ; i < 10 ; i++){
-	System.out.println("i = " + i);
+	System.out.println("i = " + i + " []");
       }
   }
 }
